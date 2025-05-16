bcc3cddc2fa2b66b2220439e9269e881575ce6f8
JefersonFreire <jfreire86@hotmail.com>
2025-05-16 14:01:10 -0300

    JsonParser finalizado

--- src/br/com/jeferson/ExchangeApiClient.java
+++ src/br/com/jeferson/ExchangeApiClient.java
@@ -1,5 +1,8 @@
 package br.com.jeferson;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,17 +13,15 @@ public class ExchangeApiClient {
 
      private static final String api_key = System.getenv("API_KEY");
 
-    public String getBody(String baseCurrency, String targetCurrency) throws IOException, InterruptedException {
-        String url = "https://v6.exchangerate-api.com/v6/pair/"+baseCurrency+"/"+targetCurrency;
+    public String getBody(String baseCurrency, String targetCurrency, double amount) throws IOException, InterruptedException {
+        String url = "https://v6.exchangerate-api.com/v6/"+api_key+"/pair/"+baseCurrency+"/"+targetCurrency+"/"+amount;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Accept", "application/json")
-                .header("Authorization", "Bearer " + api_key)
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
-
 }
